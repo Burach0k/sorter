3057c0b27788f5f416187f9782a0db424acffc7b
Burach0k <Burak_andre@mail.ru>
2018-09-19 01:37:51 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.mass = [];
+    this.comparator = function(a1,a2){
+      return (a1>a2)?1:-1;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.mass.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.mass[index];
   }
 
   get length() {
-    // your implementation
+    return this.mass.length;
   }
 
   toArray() {
-    // your implementation
+    return this.mass;
   }
 
   sort(indices) {
-    // your implementation
+    var second = [];
+    var qwe = indices;
+    indices.sort(function(a1,a2){return (a1>a2)?1:-1});
+    for(var i = 0; i<indices.length; i++){
+      second.push(this.mass[indices[i]]);
+    }
+    second.sort(this.comparator);
+    var j =0;
+    for(var i = 0; i<indices.length; i++){
+      this.mass[indices[i]] = second[i];
+      j++;
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
